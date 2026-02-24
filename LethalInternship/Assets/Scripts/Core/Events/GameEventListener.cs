6e78e9cf0cbea359cc683d39bf53385ad65e5308
Thanaloc <raphael.seguin.97@gmail.com>
2026-02-24 20:56:01 +0100

    [Add] game events

---
+++ LethalInternship/Assets/Scripts/Core/Events/GameEventListener.cs
@@ -0,0 +1,17 @@
+using Core.Events;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Core.Events
+{
+    public class GameEventListener : MonoBehaviour
+    {
+        [SerializeField] private GameEvent _GameEvent;
+        [SerializeField] private UnityEvent _Response;
+
+        private void OnEnable() => _GameEvent.RegisterListener(this);
+        private void OnDisable() => _GameEvent.UnregisterListener(this);
+
+        public void OnEventRaised() => _Response.Invoke();
+    }
+}
